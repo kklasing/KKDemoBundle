a4519f5414e2496b3288d7a9803990388d500c4b
Koen Klasing <koen@bureaublauwgeel.nl>
2014-11-07 18:15:04 +0100

    Initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace kk\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('kkDemoBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ kkDemoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace kk\DemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class kkDemoBundle extends Bundle
+{
+}
